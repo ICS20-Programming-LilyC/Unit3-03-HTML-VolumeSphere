17548d8d5a27ea8cfd49eb4fae13111f277e1a50
Lilyyyc <124380100+Lilyyyc@users.noreply.github.com>
2023-04-03 23:44:01 +0000

    Corrected spelling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2022 ZoiaB All rights reserved
+// Copyright (c) 2022 Lily C All rights reserved
 //
-// Created by: ZoiaB
-// Created on: Apr 2022
+// Created by: Lily C
+// Created on: 04/03/2023
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,5 +10,5 @@ function functionCalculate() {
   let radiusSphere = parseFloat(document.getElementById("radius").value);
   const pi = Math.PI;
   let volume = radiusSphere ** 3 * pi * 4 / 3;
-  document.getElementById("display-results").innerHTML = "The volume of the sphere is " + volume.toFixed(2) + "cm<sup>3</sup>.";
+  document.getElementById("display-results").innerHTML = "The volume of the sphere is " + volume.toFixed(2) + " cm.";
 }
